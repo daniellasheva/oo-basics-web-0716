2af37b8cb48fdd8e03eee2bcf0a0be96fdf38d53
daniellasheva <daniellasheva@gmail.com>
2016-07-28 10:01:53 -0400

    initial commit

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+    
+    def initialize (title)  #we initialize with the setter method, so now we only need the getter method
+        @title=title
+    end
+    
+    attr_reader :title
+    
+    attr_accessor :author, :page_count, :genre
+    #attr_accesor is both getter(attr_reader) and setter methods)
+    
+    
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
 
+end
 
